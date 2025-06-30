659b1dd85525201ff8ae8f8bc494b2b60ec7f55d
Paila Murali Madhav <muralipaila1313@gmail.com>
2025-06-30 09:32:30 +0530

    user

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -1,89 +1,49 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema(
+const userSchema = new mongoose.Schema(
   {
-    //username
-
     username: {
-      string: true,
+      type: String,
       required: true,
-      unique:true,
-      trim:true,
-      lowercase:true,
-      index:true
-    },
-    //    fullname
-    fullname: {
-      string: true,
-      required: true,
-      
-    },
-    // email
-    email: {
-      string: true,
-      required: true,
-      unique:true,
-      trim:true,
-
-    },
-    // mobile
-    mobile: {
-      number: true,
-      required: true,
-      unique:true,
-      minlength:10,
-      maxlength:10,
-      trim:true
-
-
-    },
-    // avtar
-    avtar: {
-      string: true,  //cloudnary url
-      required: true,
-      //default: "https://i.ibb.co/77585983/avtar.png"
-    },
-    // address
-address: {
-      string: true,
-      required: true,
-      trim:true,
-      minlength:6,
-      maxlength:100
-    },
-    // password
-    password: {
-      string: true,
-      required: true,
-      trim:true,
-      minlength:6,
-      maxlength:100
-    },
-    
-
-    // coverImage
-    coverimage:{
-      strig:true,
+      unique: true,
+      lowercase: true,
       trim: true,
-      default: "https://i.ibb.co/77585983/avtar.png"
-      },
-    // password
-    password:{
-string:true,
-required:true,
-trim:true,
-minLength:8,
-maxLength:20
+      index: true,
     },
-    //refreshtoken
-refreshtoken:{
-  type:true,
-  trim:true
-},
- }, {
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    avatar: {
+      type: String,  // cloudinary url
+      required: true,
+    },
+    coverImage: {
+      type: String,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"]
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
     timestamps: true,
   }
-
 );
 
-export default User = mongoose.model("User",UserSchema);
+
+
+
+export default User = mongoose.model("User", userSchema);
