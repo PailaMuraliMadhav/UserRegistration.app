e26d5da6601193ae0f17c510b261f61a1b02fe8c
Paila Murali Madhav <muralipaila1313@gmail.com>
2025-06-30 10:25:35 +0530

    edited user model

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -24,6 +24,28 @@ const userSchema = new mongoose.Schema(
       lowercase: true,
       trim: true,
     },
+    number:{
+      type:number,
+      trim:true,
+      maxLength:10,
+      minLength:10,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 8,
+      trim: true,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "user", "moderator"],
+      default: "user",
+    },
+    address: {
+      type: String,
+      trim: true,
+      required: true,
+    },
     avatar: {
       type: String,  // cloudinary url
       required: true,
@@ -38,13 +60,57 @@ const userSchema = new mongoose.Schema(
     refreshToken: {
       type: String,
     },
-  },
-}
+  }
+
 );
 
 
-userSchema.pre("save",async function (next){
 
+//to hash password before saving to db
+//pre save hook
+userSchema.pre("save",async function (next){
+  if(this.isModified("password")){
+    return next();
+  }
+this.password =await bcrypt.hash(this.password,10)
+next();
 })
+// to compare password with hashed password
+userSchema.methods.isPasswordCorrect = async function(){
+  return await bcrypt.compare(password,this.password);
+}
+// AccessToken
+userSchema.methods.generateAccessToken = async function(){
+  return await jwt.sign({
+    _id:this.id,
+    email: this.email,
+    username: this.username,
+    fullname:this.fullName,
+number:this.number
+  },
+  process.env.ACCESS_TOKEN_SECRET,
+  {
+    expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+  }
+  )
+  console.log("Token: ",token);
+  return token;
+}
+//REfreshToken
+userSchema.methods.generaterefreshToken =  function () {
+  const token = jwt.sign(
+    {
+      _id: this.id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+  console.log("Token: ", token);
+  return token;
+};
+
+
 
 export default User = mongoose.model("User", userSchema);
